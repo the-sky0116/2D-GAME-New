abee8840a3d702dde17df692b47ce9ac7ea59ca0
the sky <109794023+the-sky0116@users.noreply.github.com>
2023-03-10 23:40:22 +0800

    Move&Jump

--- 2D/Assets/Scripts/Player/PlayerControl.cs
+++ 2D/Assets/Scripts/Player/PlayerControl.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,25 +8,80 @@ public class PlayerControl : MonoBehaviour
 {
     public float MoveSpeed;
     public float direction;
-
+    public float JumpForce;
     Rigidbody2D rb;
+    public Transform Groundcheck;
+    public LayerMask WhatIsGround;
+    public float GroundRadius;
+    bool isWalking;
+    bool isGrounded;
+    bool isFacinghtRight;
+    Animator anim;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
         Move();
+        DirectionCheck();
+        CheckSurrounding();
     }
 
     void Update()
     {
+        Jump();
         
     }
     private void Move()
     {
         direction = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(MoveSpeed * direction, rb.velocity.y);
+        anim.SetBool("Walking",isWalking);
+    }
+    
+    private void DirectionCheck()
+    {
+        if(isFacinghtRight&&direction>0)
+        {
+            Flip();
+        }
+        else if(!isFacinghtRight&&direction<0) 
+        {
+            Flip();
+        }
+        if(rb.velocity.x!=0) 
+        {
+            isWalking= true;
+        }
+        else
+        {
+            isWalking= false;
+        }
+    }
+    private void Flip()
+    {
+        isFacinghtRight=!isFacinghtRight;
+        transform.Rotate(0.0f, 180.0f, 0.0f);
+    }
+    private void Jump() 
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if(isGrounded)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, JumpForce);
+            }          
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(Groundcheck.position, GroundRadius);
+    }
+    private void CheckSurrounding()
+    {
+        isGrounded = Physics2D.OverlapCircle(Groundcheck.position, GroundRadius, WhatIsGround);
     }
 }
